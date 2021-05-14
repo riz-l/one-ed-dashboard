7815cf51ee0e8fdbf9d86e0725552fa797875185
Tyler Rooker <tyler.rooker@dxc.com>
2021-05-14 14:41:28 +0100

    OED-86: Diagnosis Modal

--- src/app/pages/Overview/subPages/Diagnosis/Diagnosis.component.jsx
+++ src/app/pages/Overview/subPages/Diagnosis/Diagnosis.component.jsx
@@ -20,16 +20,77 @@ export default function Diagnosis() {
     patientConditions && patientConditions.length > 0
       ? patientConditions
           .filter((item) => item.category === "Diagnosis")
-          .map(({ id, problemName, status, note, ...otherPatientProps }) => (
-            <ReportEntry
-              key={id}
-              diagnosis
-              details={note !== "undefined" ? note : "No further detail"}
-              status={status}
-              type={problemName}
-              {...otherPatientProps}
-            />
-          ))
+          .map(
+            ({
+              id,
+              problemName,
+              status,
+              note,
+              verificationStatus,
+              category,
+              problemCode,
+              onsetDateTime,
+
+              ...otherPatientProps
+            }) => (
+              <ReportEntry
+                key={id}
+                diagnosis
+                details={note !== "undefined" ? note : "No further detail"}
+                status={status}
+                type={problemName}
+                openedModal={
+                  <>
+                    <Grid>
+                      <Grid.Column>
+                        <Grid.Item>
+                          <Display labelText="Note" htmlFor="note">
+                            {note}
+                          </Display>
+                        </Grid.Item>
+
+                        <Grid.Item>
+                          <Display
+                            labelText="Verification Status"
+                            htmlFor="verificationStatus"
+                          >
+                            {verificationStatus}
+                          </Display>
+                        </Grid.Item>
+                      </Grid.Column>
+
+                      <Grid.Column>
+                        <Grid.Item>
+                          <Display labelText="Category" htmlFor="category">
+                            {category}
+                          </Display>
+                        </Grid.Item>
+
+                        <Grid.Item>
+                          <Display
+                            labelText="problemCode"
+                            htmlFor="problemCode"
+                          >
+                            {problemCode}
+                          </Display>
+                        </Grid.Item>
+
+                        <Grid.Item>
+                          <Display
+                            labelText="Onset Date Time"
+                            htmlFor="onsetDateTime"
+                          >
+                            {onsetDateTime}
+                          </Display>
+                        </Grid.Item>
+                      </Grid.Column>
+                    </Grid>
+                  </>
+                }
+                {...otherPatientProps}
+              />
+            )
+          )
       : null;
 
   if (!patientConditions || patientConditions.length < 0) {
