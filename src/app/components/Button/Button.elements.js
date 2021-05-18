47d5518527493285c85c7e476bd9e36a37e610c0
thericeisready <49308672+thericeisready@users.noreply.github.com>
2021-05-18 11:12:25 +0100

    MASTER: Button margin correction

--- src/app/components/Button/Button.elements.js
+++ src/app/components/Button/Button.elements.js
@@ -22,7 +22,6 @@ export const StyledButton = styled.button`
   /* font-size: 1rem; */
   font-size: ${({ fontSize }) => (fontSize ? fontSize : "1rem")};
   justify-content: center;
-  margin-bottom: 1rem;
   padding: ${({ padding }) => (padding ? padding : "0.4rem 1rem")};
   transition: all 100ms linear;
   white-space: nowrap;
